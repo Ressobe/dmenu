faa7f2fc4482232bbfc98c543d08c7b24716d2a4
Bartosz Sobina <bartosz.sobina44@gmail.com>
2021-08-17 12:03:29 +0200

    remove center patch

--- config.h
+++ config.h
@@ -4,18 +4,20 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=12" 
+	"monospace:size=13", "fontawesome:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+
+#include "/home/relow/.cache/wal/colors-wal-dmenu.h"
+/*static const char *colors[SchemeLast][2] = {
+	//     fg         bg       
+  [SchemeNorm] = { "#c2a2bc", "#030415" },
+	[SchemeSel] = { "#c2a2bc", "#99352B" },
+	[SchemeOut] = { "#c2a2bc", "#365393" },	
 	[SchemeHp] = { "#bbbbbb", "#333333" }
-};
+};*/
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 20;
+static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words

71344b59db970fbdb318c0020b87e8afe72fe6ea
Bartosz Sobina <bartosz.sobina@tutanota.com>
2021-11-24 17:55:20 +0100

    Update

--- config.h
+++ config.h
@@ -4,17 +4,17 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=13", "fontawesome:size=15"
+	"Hack Nerd Font:size=12", "fontawesome:size=15"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 
-//#include "/home/relow/.cache/wal/colors-wal-dmenu.h"
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+#include "/home/relow/.cache/wal/colors-wal-dmenu.h"
+/*static const char *colors[SchemeLast][2] = {
+	//     fg         bg       
 	[SchemeNorm] = { "#a89984", "#282828" },
 	[SchemeSel] = { "#a89984", "#cc241d" },
 	[SchemeOut] = { "#a89984", "#689d6a" },
-};
+};*/
 /*static const char *colors[SchemeLast][2] = {
 	//     fg         bg       
   [SchemeNorm] = { "#c2a2bc", "#030415" },
